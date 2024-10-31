245eec8cfe801f8c7d4ae040442af2894efef1cc
derickferreira <derickferreira036@gmail.com>
2024-10-31 09:31:51 +0000

    feat: Create Controller

--- backend/src/server/controllers/blogs/Create.ts
+++ backend/src/server/controllers/blogs/Create.ts
@@ -1,17 +1,21 @@
+// EXPRESS
 import { Request, Response } from "express";
-
+// YUP
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares/Validations";
+// HTTP-STATUS-CODES
 import { StatusCodes } from "http-status-codes";
+// Models
+import { IBlog } from "../../database/models/Blogs";
 
-interface IBodyProps {
-  title: string;
-}
+interface IBodyProps extends Omit<IBlog, "id" | "author" | "like_amout"> {}
 
 export const createBodyValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-      title: yup.string().required().min(3).max(10),
+      title: yup.string().required().min(1).max(100),
+      description: yup.string().required().min(5).max(1000),
+      image: yup.string().required().url(),
     })
   ),
 }));
@@ -20,7 +24,9 @@ export const create = async (
   request: Request<{}, {}, IBodyProps>,
   response: Response
 ) => {
-  console.log(request.body.title);
+  const { title, description, image } = request.body;
 
-  return response.status(StatusCodes.CREATED).json("create");
+  return response
+    .status(StatusCodes.CREATED)
+    .json({ blog: { title, description, image } });
 };
