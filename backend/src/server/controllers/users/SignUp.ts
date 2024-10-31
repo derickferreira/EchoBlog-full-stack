09b23e01fde1c3edcecbcf409b7020c755995922
derickferreira <derickferreira036@gmail.com>
2024-10-31 10:54:27 +0000

    feat: Creating User's controllers

--- backend/src/server/controllers/users/SignUp.ts
+++ backend/src/server/controllers/users/SignUp.ts
@@ -1,9 +1,27 @@
+// EXPRESS
 import { Request, Response } from "express";
-
+// YUP
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares/Validations";
+// HTTP-STATUS-CODES
 import { StatusCodes } from "http-status-codes";
+// MODELS
+import {IUser} from "../../database/models"
+// INTERFACE
+interface IBodyProps extends Omit<IUser, "id">{}
 
-export const SignUp = (request: Request, response: Response) => {
+// export const signUpValidation = validation((getSchema) => ({
+//   body: getSchema<IBodyProps>(
+//     yup.object().shape({
+//       name: yup.string(),
+//       nickname: yup.string(),
+//       email: yup.string().email(),
+//       password: yup.string(),
+
+//     })
+//   )
+// }))
+
+export const SignUp = (request: Request<{}, {}, IBodyProps>, response: Response) => {
   return response.status(StatusCodes.CREATED).json("signup");
 };
