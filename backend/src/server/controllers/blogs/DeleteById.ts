8e149a38348edd1b1a8ce8e97af62ce316604f02
derickferreira <derickferreira036@gmail.com>
2024-11-01 13:32:00 +0000

    feat: DeleteById controller

--- backend/src/server/controllers/blogs/DeleteById.ts
+++ backend/src/server/controllers/blogs/DeleteById.ts
@@ -6,6 +6,18 @@ import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares/Validations";
 
+interface IParamsProps {
+  id?: number;
+}
+
+export const deleteByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamsProps>(
+    yup.object().shape({
+      id: yup.number().integer().positive().required().moreThan(0),
+    })
+  ),
+}));
+
 export const deleteById = async (request: Request, response: Response) => {
   return response.status(StatusCodes.NO_CONTENT).send("deleteById");
 };
