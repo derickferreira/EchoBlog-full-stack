0dc5488883b3e93cfdb9cef170af85a7e1f09f86
derickferreira <derickferreira036@gmail.com>
2024-11-01 14:21:56 +0000

    feat: comments/GetAll Controller

--- backend/src/server/controllers/comments/GetAll.ts
+++ backend/src/server/controllers/comments/GetAll.ts
@@ -2,7 +2,27 @@
 import { Request, response, Response } from "express";
 // HTTP-STATUS-CODE
 import { StatusCodes } from "http-status-codes";
-
-export const getAll = async (request: Request, response: Response) => {
-    return response.status(StatusCodes.OK).json("getAll")
+// YUP
+import * as yup from "yup";
+import { validation } from "../../shared/middlewares/Validations";
+// INTERFACE
+interface IQueryProps {
+  limit?: number;
 }
+
+export const getAllValidation = validation((getSchema) => ({
+  query: getSchema<IQueryProps>(
+    yup.object().shape({
+      limit: yup.number().integer().min(1).max(100), // default is 10
+    })
+  ),
+}));
+
+export const getAll = async (
+  request: Request<{}, {}, {}, IQueryProps>,
+  response: Response
+) => {
+  const { limit } = request.query;
+
+  return response.status(StatusCodes.OK).json(limit);
+};
