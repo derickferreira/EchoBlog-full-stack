80ac4d124f386bfc53fa6127f4d7d53ad6cec395
derickferreira <derickferreira036@gmail.com>
2024-11-01 14:07:23 +0000

    feat: SignIn Controller

--- backend/src/server/controllers/users/SignIn.ts
+++ backend/src/server/controllers/users/SignIn.ts
@@ -1,9 +1,30 @@
+// EXPRESS
 import { Request, Response } from "express";
-
+// YUP
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares/Validations";
+// HTTP-STATUS-CODES
 import { StatusCodes } from "http-status-codes";
+// INTERFACE
+interface IBodyProps {
+  email: string;
+  password: string;
+}
 
-export const signIn = (request: Request, response: Response) => {
-  return response.status(StatusCodes.OK).json("signIn");
+export const signInValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(
+    yup.object().shape({
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    })
+  ),
+}));
+
+export const signIn = (
+  request: Request<{}, {}, IBodyProps>,
+  response: Response
+) => {
+  const { email, password } = request.body;
+
+  return response.status(StatusCodes.OK).json({ user: { email, password } });
 };
