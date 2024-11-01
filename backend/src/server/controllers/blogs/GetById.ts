46c62f0feaaf2b1a6912faefe169252e5194e5cb
derickferreira <derickferreira036@gmail.com>
2024-11-01 13:41:37 +0000

    feat: GetById controller

--- backend/src/server/controllers/blogs/GetById.ts
+++ backend/src/server/controllers/blogs/GetById.ts
@@ -2,7 +2,27 @@
 import { Request, Response } from "express";
 // HTTP-STATUS-CODE
 import { StatusCodes } from "http-status-codes";
+// YUP
+import * as yup from "yup";
+import { validation } from "../../shared/middlewares/Validations";
+// INTERFACE
+interface IParamsProps {
+  id?: number;
+}
 
-export const getById = async (request: Request, response: Response) => {
-  return response.status(StatusCodes.OK).json({ title: "getById" });
+export const getByIdValidations = validation((getSchema) => ({
+  params: getSchema<IParamsProps>(
+    yup.object().shape({
+      id: yup.number().integer().moreThan(0),
+    })
+  ),
+}));
+
+export const getById = async (
+  request: Request<IParamsProps>,
+  response: Response
+) => {
+  return response
+    .status(StatusCodes.OK)
+    .json({ title: request.params.id, name: "Leighton Buzzard" });
 };
