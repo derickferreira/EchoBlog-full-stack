9eca93154e3536fe34ad622fd625a5fcc5582682
derickferreira <derickferreira036@gmail.com>
2024-11-01 13:55:08 +0000

    feat: SignUp controller

--- backend/src/server/controllers/users/SignUp.ts
+++ backend/src/server/controllers/users/SignUp.ts
@@ -6,22 +6,29 @@ import { validation } from "../../shared/middlewares/Validations";
 // HTTP-STATUS-CODES
 import { StatusCodes } from "http-status-codes";
 // MODELS
-import {IUser} from "../../database/models"
+import { IUser } from "../../database/models";
 // INTERFACE
-interface IBodyProps extends Omit<IUser, "id">{}
+interface IBodyProps extends Omit<IUser, "id" | "create_at" | "blogs"> {}
 
-// export const signUpValidation = validation((getSchema) => ({
-//   body: getSchema<IBodyProps>(
-//     yup.object().shape({
-//       name: yup.string(),
-//       nickname: yup.string(),
-//       email: yup.string().email(),
-//       password: yup.string(),
+export const signUpValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(
+    yup.object().shape({
+      name: yup.string().required().min(2).max(50),
+      nickname: yup.string().required().min(2).max(50),
+      email: yup.string().email().required(),
+      password: yup.string().required().min(8).max(100),
+      profile_url: yup.string().url().optional(),
+    })
+  ),
+}));
 
-//     })
-//   )
-// }))
+export const SignUp = (
+  request: Request<{}, {}, IBodyProps>,
+  response: Response
+) => {
+  const { name, nickname, email, password, profile_url } = request.body;
 
-export const SignUp = (request: Request<{}, {}, IBodyProps>, response: Response) => {
-  return response.status(StatusCodes.CREATED).json("signup");
+  return response
+    .status(StatusCodes.CREATED)
+    .json({ user: { name, nickname, email, password, profile_url } });
 };
