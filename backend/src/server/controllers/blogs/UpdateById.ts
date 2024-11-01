ff140083886d6703669ef702f60f2ac4f9ce52a3
derickferreira <derickferreira036@gmail.com>
2024-11-01 13:45:12 +0000

    feat: UpdateById controller

--- backend/src/server/controllers/blogs/UpdateById.ts
+++ backend/src/server/controllers/blogs/UpdateById.ts
@@ -2,7 +2,31 @@
 import { Request, Response } from "express";
 // HTTP-STATUS-CODE
 import { StatusCodes } from "http-status-codes";
+// YUP
+import * as yup from "yup";
+import { validation } from "../../shared/middlewares/Validations";
+// INTERFACE
+interface IParamsProps {
+  id?: number;
+}
 
-export const updateById = async (request: Request, response: Response) => {
+interface IBodyProps {
+  name?: string;
+}
+
+export const updateByIdValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(
+    yup.object().shape({
+      name: yup.string().required().min(3).max(10),
+    })
+  ),
+  params: getSchema<IParamsProps>(
+    yup.object().shape({
+      id: yup.number().required().moreThan(0),
+    })
+  ),
+}));
+
+export const updateById = async (request: Request<IParamsProps, {}, IBodyProps>, response: Response) => {
   return response.status(StatusCodes.NO_CONTENT).json("updateById");
 };
