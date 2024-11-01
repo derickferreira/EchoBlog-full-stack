27c7d05f834513a12f451766c0a2c129188d824e
derickferreira <derickferreira036@gmail.com>
2024-11-01 13:36:50 +0000

    feat: GetAll controller

--- backend/src/server/controllers/blogs/GetAll.ts
+++ backend/src/server/controllers/blogs/GetAll.ts
@@ -2,7 +2,32 @@
 import { Request, Response } from "express";
 // HTTP-STATUS-CODE
 import { StatusCodes } from "http-status-codes";
+// YUP
+import * as yup from "yup";
+import { validation } from "../../shared/middlewares/Validations";
+// INTERFACE
+interface IQueryProps {
+  page?: number;
+  limit?: number;
+  filter?: string;
+}
 
-export const getAll = async (request: Request, response: Response) => {
+export const getAllValidation = validation((getSchema) => ({
+  params: getSchema<IQueryProps>(
+    yup.object().shape({
+      page: yup.number().notRequired().moreThan(0).nonNullable(),
+      limit: yup.number().notRequired().moreThan(0).nonNullable(),
+      filter: yup.string().notRequired().nonNullable(),
+    })
+  ),
+}));
+
+export const getAll = async (
+  request: Request<{}, {}, {}, IQueryProps>,
+  response: Response
+) => {
+  response.setHeader("access-control-expose-headers", "x-total-count");
+  response.setHeader("x-total-count", 1);
+
   return response.status(StatusCodes.OK).json({ title: "getAll" });
 };
